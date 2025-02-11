bc654b01f076181f74ef8935d18270a2050a96cf
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-11 19:51:17 +0200

    added pageBaseUrl as env

--- client/src/utils/general.js
+++ client/src/utils/general.js
@@ -1,25 +1,25 @@
-export const getObjHandleForm=(e)=>{
-    e.preventDefault()
-    const formData=new FormData( e.target)
-    const formObj= Object.fromEntries(formData)
-    return formObj
-}
-
-export const postOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    mode:'cors'
-  };
-export const pageBaseUrl="http://localhost:4200/api/v1/";
-
-export const getOptions = {
-  method: 'Get',
-  headers: { 'Content-Type': 'application/json' },
-  mode:'cors'
+export const getObjHandleForm = (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const formObj = Object.fromEntries(formData);
+  return formObj;
 };
 
-export const optionsWithToken=(anyOption,tokenStr)=>{
-  const getOptionsCopy={...anyOption}
-  getOptionsCopy.headers.Authorization=`Bearer ${tokenStr}`
-  return getOptionsCopy
-}
+export const postOptions = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  mode: "cors",
+};
+export const pageBaseUrl = import.meta.env.VITE_SERVER_URL + "api/v1/";
+
+export const getOptions = {
+  method: "Get",
+  headers: { "Content-Type": "application/json" },
+  mode: "cors",
+};
+
+export const optionsWithToken = (anyOption, tokenStr) => {
+  const getOptionsCopy = { ...anyOption };
+  getOptionsCopy.headers.Authorization = `Bearer ${tokenStr}`;
+  return getOptionsCopy;
+};
